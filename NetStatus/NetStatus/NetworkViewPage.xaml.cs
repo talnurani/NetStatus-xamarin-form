003369ae13d6fd381265a5eedf5b344923f5ca4a
talnurani <talnurani@gmail.com>
2017-05-15 19:57:42 +0300

    some codes...

---
+++ NetStatus/NetStatus/NetworkViewPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Plugin.Connectivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Plugin.Connectivity.Abstractions;
+
+namespace NetStatus
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NetworkViewPage : ContentPage
+    {
+        public NetworkViewPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (ConnectionDetails.Text == null)
+                return;
+            ConnectionDetails.Text = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault().ToString();
+            CrossConnectivity.Current.ConnectivityChanged += UpdateNetworkInfo;
+        }
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            if (CrossConnectivity.Current != null)
+            {
+                CrossConnectivity.Current.ConnectivityChanged -= UpdateNetworkInfo;
+            }
+        }
+        private void UpdateNetworkInfo(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (CrossConnectivity.Current!=null && CrossConnectivity.Current.ConnectionTypes != null)
+            {
+                var connectionType = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault();
+                ConnectionDetails.Text = connectionType.ToString();
+            }
+        }
+    }
+}
---
+++ NetStatus/NetStatus/NoNetworkPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NetStatus
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoNetworkPage : ContentPage
+    {
+        public NoNetworkPage()
+        {
+            InitializeComponent();
+            BackgroundColor= Color.FromRgb(0xf0, 0xf0, 0xf0);
+            Content = new Label()
+            {
+                Text = "No network Connection Available",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                TextColor = Color.FromRgb(0x40, 0x40, 0x40)
+            };
+        }
+    }
+}
